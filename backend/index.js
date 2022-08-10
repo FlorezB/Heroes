6e22e51a95d7dd2a8ee0afd8566874955e994dde
Caroline <alyseflorez@hotmail.fr>
2022-08-10 15:41:15 +0200

    initialisation

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const port = 5000
+
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on ${5000}`)
+})
